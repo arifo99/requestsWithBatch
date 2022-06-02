99ff945ae327b2da7851992bf871ed30541e6ff6
arifo99 <106638102+arifo99@users.noreply.github.com>
2022-06-02 02:41:18 +0100

    changed name from requestsW to requests in test file

--- testBatches.py
+++ testBatches.py
@@ -1,10 +1,10 @@
-import requestsW
+import requests
 
 url="https://www.youtube.com/watch?v=ydUr87glh80"
 args = []
 args.append({"method": "GET", "url": "https://www.bing.com/search?q=bananas", "kwargs": {"headers": {"test": "1"}}})
 args.append({"method": "GET", "url": "https://www.bing.com/search?q=bananas&first=11", "kwargs": {"headers": {"test": "1"}}})
-r = requestsW.batch(args)
+r = requests.batch(args)
 
 print(r[0].text)
 print(r[1].text)
